4eab464e8db48d93ec4ad9ed0a1c7fb06bc08a55
Lucas França <lucasfrancaid@gmail.com>
2021-03-23 01:37:09 -0300

    Add mySEO config

---
+++ config/mySEO.js
@@ -0,0 +1,12 @@
+export default function mySEO() {
+	const config = {
+		'url': 'https://lucasfrancaid.com.br/',
+		'title': 'Lucas França | Software Developer',
+		'description': `
+			Software Developer since 2019.
+			I can develop a system from zero to deploy.
+			Able to use many technologies, in special Python, NodeJS, FastAPI, Django, React, Vue, AWS and Linux.
+		`,
+	}
+	return config
+}
